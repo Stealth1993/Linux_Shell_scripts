04dc1da62de50a844694483fae689f29e2b8cf76
Jha Santosh <76648814+Stealth1993@users.noreply.github.com>
2025-01-23 05:54:25 -0800

    Add files via upload
    Get the latest xkcd wallpaper.

---
+++ xkcd.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash 
+
+# xkcdsh
+# info: 
+# 		grabs the latest xkcd comic and displays it with sixel graphics
+# Dependency:
+# 		libsixel
+# 		curl
+# 		GNU sed
+# 		GNU Grep
+
+curl -s https://xkcd.com | grep -oP '(?<=<img src="//imgs.xkcd.com/comics/)[^"]*' | \
+	sed 's/^/https:\/\/imgs.xkcd.com\/comics\//' | \
+	while read -r url; do
+		filename=$(basename "$url")
+		curl -so "$filename" "$url" && img2sixel "$filename"
+		printf '%s\n' "$filename"
+	done
